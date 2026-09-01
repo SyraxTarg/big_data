ddbc8cb7d82f7110d9872e330569a2ff515fe02c
SyraxTarg <maimiti.saint-marc@efrei.net>
2026-09-01 13:48:42 +0200

    finished silver

--- src/scripts/silver.py
+++ src/scripts/silver.py
@@ -41,21 +41,41 @@ def copy_tables():
 
     logging.info("COPYING DIAGNOSTICS")
     clickhouse_client.command(f'DROP TABLE IF EXISTS chu.diagnostics_silver')
+    clickhouse_client.command(f'''
+                CREATE TABLE IF NOT EXISTS chu.diagnostics_silver (
+                    patient_id String,
+                    code_cim10 String
+                )
+                ENGINE = MergeTree()
+                ORDER BY patient_id
+            ''')
     clickhouse_client.query(f'''
-        CREATE TABLE chu.diagnostics_silver AS chu.diagnostics_bronze
+        INSERT INTO chu.diagnostics_silver SELECT "chu"."patients_bronze"."patient_id" AS "patient_id", "chu"."diagnostics_bronze"."code_cim10" FROM "chu"."diagnostics_bronze" 
+        INNER JOIN "chu"."sejours_bronze" ON "chu"."diagnostics_bronze"."stay_id" = "chu"."sejours_bronze"."stay_id"
+        INNER JOIN "chu"."patients_bronze" ON "chu"."sejours_bronze"."patient_id" = "chu"."patients_bronze"."patient_id"
     ''')
-    clickhouse_client.query(f'''
-        INSERT INTO chu.diagnostics_silver SELECT * FROM chu.diagnostics_bronze;
-    ''')
+
 
     logging.info("COPYING MONITORING")
     clickhouse_client.command(f'DROP TABLE IF EXISTS chu.monitoring_silver')
-    clickhouse_client.query(f'''
-        CREATE TABLE chu.monitoring_silver AS chu.monitoring_bronze
-    ''')
-    clickhouse_client.query(f'''
-        INSERT INTO chu.monitoring_silver SELECT * FROM chu.monitoring_bronze;
-    ''')
+    clickhouse_client.command(f'''
+                CREATE TABLE IF NOT EXISTS chu.monitoring_silver (
+                    patient_id String,
+                    ts date,
+                    heart_rate Int,
+                    spo2 Int,
+                    temp_c Float
+                )
+                ENGINE = MergeTree()
+                ORDER BY patient_id
+            ''')
+    clickhouse_client.command('''INSERT INTO chu.monitoring_silver SELECT "chu"."patients_bronze"."patient_id" AS "patient_id",
+                                  "chu"."monitoring_bronze"."ts",
+                                  "chu"."monitoring_bronze"."heart_rate",
+                                  "chu"."monitoring_bronze"."spo2",
+                                  "chu"."monitoring_bronze"."temp_c" FROM "chu"."monitoring_bronze"
+                                  INNER JOIN "chu"."sejours_bronze" ON "chu"."monitoring_bronze"."stay_id" = "chu"."sejours_bronze"."stay_id"
+                                  INNER JOIN "chu"."patients_bronze" ON "chu"."sejours_bronze"."patient_id" = "chu"."patients_bronze"."patient_id"''')
 
 def main():
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
