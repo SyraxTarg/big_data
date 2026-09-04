58d4691e09d9d3df67b8f9e945ecbc621642f502
SyraxTarg <maimiti.saint-marc@efrei.net>
2026-09-04 12:22:53 +0200

    exception handling

--- src/scripts/scheduler.py
+++ src/scripts/scheduler.py
@@ -1,15 +1,20 @@
 import schedule
 import time
 from scripts.cron import main as cron
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 def main():
-    # schedule.every(1).minutes.do(cron)   # TESTING
-    schedule.every().day.at("01:00").do(cron)
+    try:
+        # schedule.every(1).minutes.do(cron)   # TESTING
+        schedule.every().day.at("01:00").do(cron)
 
-    while 1:
-        schedule.run_pending()
-        time.sleep(1)
+        while 1:
+            schedule.run_pending()
+            time.sleep(1)
+    except Exception as e:
+        logging.error(f'Une erreur est survenue lors de l\'exécution du job: {e}')
 
 if __name__ == "__main__":
     main()
